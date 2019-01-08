6a789e99b1478a6cc5b92d8fcdf949f84bb2a457
Kyle Powers <kyle.powers103@gmail.com>
2019-01-08 01:59:44 -0500

    almost there

---
+++ loader.py
@@ -0,0 +1,8 @@
+# file loader
+
+# import "weather_data/"
+# import "Wea"
+# import 
+# import FlatironProject3FlightDelayPredictions
+# import FlatironProject3FlightDelayPredictions/'FlightDataIntroNotebook'
+# FlatironProject3FlightDelayPredictions
